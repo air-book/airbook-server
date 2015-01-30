e2847b44b9c6b31de1bd4218a3626a608ebe9168
Mauro Bianchi <bianchimro@gmail.com>
2015-01-30 14:32:12 +0100

    models for books

--- airbook_server/books/models.py
+++ airbook_server/books/models.py
@@ -1,15 +1,34 @@
 from django.db import models
+#from django.contrib.auth.models import User
 
 
 class BookShop(models.Model):
     name = models.CharField(max_length=200)
 
 
+class BookAuthor(models.Model):
+    author = models.CharField(max_length=200)
+
+
+class BookCategory(models.Model):
+    category = models.CharField(max_lenght=16, choices=BOOK_CATEGORIES)
+
+
 class Book(models.Model):
     bookshop = models.ForeignKey(BookShop)
     title = models.CharField(max_length=200)
-    publication_year = models.IntegerField()
+    description = models.CharField(max_length=2000)
+    categories = models.ManyToManyField(BookCategory)
+    authors = models.ManyToManyField(BookAuthor)
+    price = models.FloatField()
+    publication_year = models.IntegerField(null=True, blank=True)
+    editor = models.CharField(max_length=200)
+    isbn_code = models.Charfield(max_length=100, null=True, blank=True)
 
+
+class BookImage(models.Model):
+    book = models.ForeignKey(Book)
+    image = models.ImageField(upload_to="book_images", related_name="images")
 
 
 
