fa1cb5212026a05ab9d07556607a0ce3b3571b1f
mrjungle <davide.nuccio@gmail.com>
2015-02-13 17:41:20 +0100

    aggiunti i null=true nei campi manytomany del model

---
+++ airbook_server/books/migrations/0008_auto_20150213_1640.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('books', '0007_book_conditions'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='book',
+            name='authors',
+            field=models.ManyToManyField(to='books.BookAuthor', null=True, blank=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='book',
+            name='categories',
+            field=models.ManyToManyField(to='books.BookCategory', null=True, blank=True),
+            preserve_default=True,
+        ),
+    ]
